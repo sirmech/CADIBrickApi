8a5fc19e797d4c18dad8fc304db0052cf303011d
Hamish MacKay <haymitchmackay@live.co.uk>
2018-07-10 23:22:45 +0100

    added comments to order controller
    to clarify what the class is doing

--- src/main/java/hello/OrderController.java
+++ src/main/java/hello/OrderController.java
@@ -11,9 +11,12 @@ public class OrderController {
 
     @RequestMapping("/order/create")
     public String placeOrder(@RequestParam(value="amount", defaultValue="0") String amount) {
-    	UUID orderRefrence = UUID.randomUUID();
+    	//Generate unique order reference
+    	UUID orderRefrence = UUID.randomUUID(); 
+    	//Create a new order the amount received
     	Order newOrder = new Order(Integer.parseInt(amount),orderRefrence.toString());
-    	//store order
+    	//store the new order in the orders list
+    	//return order reference
         return orderRefrence.toString();
     }
 	
